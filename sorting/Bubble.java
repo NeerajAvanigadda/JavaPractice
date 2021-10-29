a83d830a720aee7f95af22b4051749341f9c7f7c
NeerajAvanigadda <neerajavanigadda1234@gmail.com>
2021-10-29 23:16:01 +0530

    Bubble sort

---
+++ sorting/Bubble.java
@@ -0,0 +1,54 @@
+package sorting;
+
+public class Bubble {
+
+    /*
+    * input: 3,6,2,8,3,7
+    * output: 2,3,3,,6,7,8
+    * goal ascending order
+    *
+    * */
+    public static void main(String... args)
+    {
+        /*
+        * int - data type
+        * Integer [I] = -negative ......0.....positive
+        * eg: -infinity......-1000,-999......-3,-2,-1,0,1,2,3..........+infinity
+        *
+        *int[] - int array , [] subscripts
+        *
+        * int[]array = {1,2,3}
+        * new -provides space
+        *
+        * int[size/length/quatity]
+        * int[] array = new int[10];
+        * */
+
+        int[] array = {3,6,2,8,3,7};
+
+
+        //Bubble sort
+
+        int x=0;
+
+
+        for(int i=0;i<=10;)
+        {
+            array[i]
+        }
+
+
+
+
+
+        int i;
+        if(array[i]>array[i+1])
+        {
+            int xtra;
+            xtra= array[i+1];
+            array[i+1] =array[i];
+            array[i] =xtra;
+        }
+
+    }
+}
